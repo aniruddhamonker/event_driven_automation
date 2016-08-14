8cea8320ccea963da6f6a8823dce580af24bc1c2
aniruddhamonker <aniruddh.amonker@live.com>
2016-08-13 18:11:10 -0700

    Restructuring spaces

--- sensors/VDXLowDiskAlertSensor.py
+++ sensors/VDXLowDiskAlertSensor.py
@@ -6,60 +6,57 @@ import re
 import pprint
 from st2reactor.sensor.base import Sensor
 
-if __name__ == '__main__':
+class SyslogSensor(Sensor):
 
-	class SyslogSensor(Sensor):
+    def __init__(self):
+        super(SyslogSensor, self).__init__(sensor_service=sensor_service)
+        self._trigger_ref = 'NOS.LowDiskAlert'
 
-	    def __init__(self):
-	        super(SyslogSensor, self).__init__(sensor_service=sensor_service)
-		self._trigger_ref = 'NOS.LowDiskAlert'
+    def setup(self):
+        self.ServerSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        try:
+            self.ServerSocket.bind(('0.0.0.0',514))
+            print("Server binding Successful \n")
+        except Exception as e:
+            print "Error Binding the server:",type(e),e
 
-	    def setup(self):	
-	        self.ServerSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-		try:
-		    self.ServerSocket.bind(('0.0.0.0',514))
-	            print("Server binding Successful \n")
-		except Exception as e:
-                    print "Error Binding the server:",type(e),e
-			
-	    def run(self):
-	    	while True:
-		    data,self.ClientAddress = self.ServerSocket.recvfrom(65535)
-		    FilterData = re.search('<\d+>(?P<DATE>\w+\s+\d{1,2} [\d\+:]+).*(?P<MSG_ID>\[msgid@.*?\]).*(?P<DEVICE>\[attr@.*?\]).*(?P<SEVERITY>\[severity@.*?\]).*\[swname@.*value=(?P<HOSTNAME>\".+?\")\].*BOM\s?(?P<SYSLOG>.*)',str(data))	
-			
-	            try:
-		        if 'FW-1402' in FilterData.group('MSG_ID'):
-        	            print ("Low Disk Space reported on Switch with IP address %s\n")%(self.ClientAddress)
-                	    self.TriggerLowDiskAlert()
-		    except Exception as e:
-                        pass
+    def run(self):
+        while True:
+            data,self.ClientAddress = self.ServerSocket.recvfrom(65535)
+            FilterData = re.search('<\d+>(?P<DATE>\w+\s+\d{1,2} [\d\+:]+).*(?P<MSG_ID>\[msgid@.*?\]).*(?P<DEVICE>\[attr@.*?\]).*(?P<SEVERITY>\[severity@.*?\]).*\[swname@.*value=(?P<HOSTNAME>\".+?\")\].*BOM\s?(?P<SYSLOG>.*)',str(data))
 
- 		    time.sleep(0.5)
+            try:
+                if 'FW-1402' in FilterData.group('MSG_ID'):
+                    print ("Low Disk Space reported on Switch with IP address %s\n")%(self.ClientAddress)
+                    self.TriggerLowDiskAlert()
+            except Exception as e:
+                    pass
 
-			
-	    def TriggerLowDiskAlert(self):
-	        trigger = self._trigger_ref
-		payload = {
-		 	      'ClientAddress' : self.ClientAddress
-			  } 
+            time.sleep(0.5)
 
-		try:
-		    self.sensor_service.dispatch(trigger=trigger,payload=payload)
-		except Exception as e:
-		    print "Failed to dispatch Trigger:",type(e),e
+    def TriggerLowDiskAlert(self):
+        trigger = self._trigger_ref
+        payload = {
+                      'ClientAddress' : self.ClientAddress
+                  }
 
-            def cleanup(self):
-	        self.ServerSocket.close()
+        try:
+            self.sensor_service.dispatch(trigger=trigger,payload=payload)
+        except Exception as e:
+            print "Failed to dispatch Trigger:",type(e),e
 
-	    def add_trigger(self, trigger):
-	        pass
+    def cleanup(self):
+        self.ServerSocket.close()
 
-            def update_trigger(self, trigger):
-	        # This method is called when trigger is updated
-		pass
+    def add_trigger(self, trigger):
+        pass
 
-	    def remove_trigger(self, trigger):
-	        # This method is called when trigger is deleted
-		pass
+    def update_trigger(self, trigger):
+# This method is called when trigger is updated
+        pass
+
+    def remove_trigger(self, trigger):
+# This method is called when trigger is deleted
+        pass
 
 
